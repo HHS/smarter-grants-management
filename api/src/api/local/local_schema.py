7b69ef873eb575a7161c1ed7e7ea14e5499b8bae
Michael Chouinard <46358556+chouinar@users.noreply.github.com>
2026-08-17 12:39:26 -0400

    [Issue #11877] Add local user endpoint (#29)
    ## Summary
    Fixes https://github.com/HHS/simpler-grants-gov/issues/11877
    
    ## Changes proposed
    * Adds a local-only endpoint for fetching users
    
    ## Context for reviewers
    This is largely copied as-is from the Simpler grants repo, our frontend
    uses this for getting users locally to easily switch between them.
    
    Only meaningful changes are:
    * We don't have first/last name yet, so they're just always null
    * I modified the tests to avoid the performance issues we see in Simpler
    
    ## Validation steps
    Verified endpoint works locally, can fetch the data uneventfully

---
+++ api/src/api/local/local_schema.py
@@ -0,0 +1,40 @@
+from grants_shared.api.schemas.extension import Schema, fields
+from grants_shared.api.schemas.response_schema import AbstractResponseSchema
+
+
+class LocalUserTokenSchema(Schema):
+
+    user_id = fields.UUID(metadata={"description": "The ID of the user"})
+
+    first_name = fields.String(
+        metadata={"description": "The first name of the user", "example": "Bob"}, allow_none=True
+    )
+    last_name = fields.String(
+        metadata={"description": "The last name of the user", "example": "Smith"}, allow_none=True
+    )
+
+    oauth_id = fields.String(
+        metadata={
+            "description": "The OAuth id of the user - can be used to login locally via our mock OAuth server",
+            "example": "my_example_user",
+        },
+        allow_none=True,
+    )
+    user_jwt = fields.String(
+        metadata={
+            "description": "The JWT of the user, can be used with our X-MGMT-Token auth",
+            "example": "abc123",
+        },
+        allow_none=True,
+    )
+    user_api_key = fields.String(
+        metadata={
+            "description": "The API key of the user, can be used with our X-API-Key auth",
+            "example": "my_example_user_token",
+        },
+        allow_none=True,
+    )
+
+
+class LocalUserTokenResponseSchema(AbstractResponseSchema):
+    data = fields.List(fields.Nested(LocalUserTokenSchema))
