83199d5b9a9f2c0dd8bd57e914fd85f8c8077e37
Nathan Stricker <119358383+nathan-stricker@users.noreply.github.com>
2026-08-20 14:40:13 -0400

    [Issue #11827] Create initial roles for grantor authorization (#42)
    ## Summary
    
    <!-- Use "Fixes" to automatically close issue upon PR merge. Use "Work
    for" when UAT is required. -->
    Work for #11827 ([Link to issue in
    SGG](https://github.com/HHS/simpler-grants-gov/issues/11827))
    
    ## Changes proposed
    
    <!-- What was added, updated, or removed in this PR. -->
    - Defined the four core grantor AuthZ roles (Partner Admin, Partner
    Viewer, Grantor Organization Admin, Organization Viewer) using
    `build_role`
    - Updated tests
    
    ## Context for reviewers
    
    <!-- Technical or background context, more in-depth details of the
    implementation, and anything else you'd like reviewers to know about
    that will help them understand the changes in the PR. -->
    Roles and privileges set up as specified in the table
    [here](https://navasage.atlassian.net/wiki/spaces/Grantsgov/pages/3397189690/Grantor+AuthZ+Modifications+August+3rd+2026#Roles)
    
    ## Validation steps
    
    <!-- Manual testing instructions, as well as any helpful references
    (screenshots, GIF demos, code examples or output). -->
    - Validate all tests pass

--- api/tests/constants/test_static_role_values.py
+++ api/tests/constants/test_static_role_values.py
@@ -1,13 +1,46 @@
+from src.constants.lookup_constants import Privilege, ResourceType
 from src.constants.static_role_values import CORE_ROLES
 
 # The privileges each core role is expected to grant, keyed by role name.
-EXPECTED_ROLE_PRIVILEGES = {}
+EXPECTED_ROLE_PRIVILEGES = {
+    "Partner Admin": {
+        Privilege.VIEW_PARTNER,
+        Privilege.MANAGE_PARTNER_MEMBERS,
+        Privilege.UPDATE_PARTNER,
+        Privilege.VIEW_PROGRAM,
+        Privilege.UPDATE_PROGRAM,
+        Privilege.VIEW_GRANTOR_ORGANIZATION,
+        Privilege.UPDATE_GRANTOR_ORGANIZATION,
+        Privilege.MANAGE_GRANTOR_ORGANIZATION_MEMBERS,
+    },
+    "Partner Viewer": {
+        Privilege.VIEW_PARTNER,
+        Privilege.VIEW_PROGRAM,
+        Privilege.VIEW_GRANTOR_ORGANIZATION,
+    },
+    "Grantor Organization Admin": {
+        Privilege.VIEW_PROGRAM,
+        Privilege.UPDATE_PROGRAM,
+        Privilege.VIEW_GRANTOR_ORGANIZATION,
+        Privilege.UPDATE_GRANTOR_ORGANIZATION,
+        Privilege.MANAGE_GRANTOR_ORGANIZATION_MEMBERS,
+    },
+    "Organization Viewer": {
+        Privilege.VIEW_PROGRAM,
+        Privilege.VIEW_GRANTOR_ORGANIZATION,
+    },
+}
 
-EXPECTED_ROLE_RESOURCE_TYPES = {}
+EXPECTED_ROLE_RESOURCE_TYPES = {
+    "Partner Admin": {ResourceType.PARTNER},
+    "Partner Viewer": {ResourceType.PARTNER},
+    "Grantor Organization Admin": {ResourceType.GRANTOR_ORGANIZATION},
+    "Organization Viewer": {ResourceType.GRANTOR_ORGANIZATION},
+}
 
 
-def test_core_roles_defines_exactly_seven_roles():
-    assert len(CORE_ROLES) == 0
+def test_core_roles_defines_exactly_four_roles():
+    assert len(CORE_ROLES) == 4
     assert {role.role_name for role in CORE_ROLES} == set(EXPECTED_ROLE_PRIVILEGES.keys())
 
 
