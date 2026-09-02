8370b7eda8768baa5541a35aee8d2793acd43072
Michael Chouinard <chouinar6@gmail.com>
2026-09-02 13:48:38 -0400

    [Issue #12097] Update sam.gov client to fetch ALNs

--- api/src/adapters/sam_gov/factory.py
+++ api/src/adapters/sam_gov/factory.py
@@ -1,8 +1,49 @@
 """Factory for creating SAM.gov clients."""
 
+import logging
+from datetime import datetime
+
+import pytz
+
 from src.adapters.sam_gov.client import BaseSamGovClient, SamGovClient
 from src.adapters.sam_gov.config import SamGovConfig
 from src.adapters.sam_gov.mock_client import MockSamGovClient
+from src.adapters.sam_gov.models import SamAssistanceListingData
+
+logger = logging.getLogger(__name__)
+
+MOCK_ASSISTANCE_LISTINGS = [
+    SamAssistanceListingData(
+        assistanceListingId="10.207",
+        title="Animal Health and Disease Research",
+        status="Active",
+        publishedDate=datetime(2026, 7, 30, 12, 0, 0, tzinfo=pytz.utc),
+    ),
+    SamAssistanceListingData(
+        assistanceListingId="43.007",
+        title="Space Operations",
+        status="Active",
+        publishedDate=datetime(2010, 11, 1, 11, 33, 22, tzinfo=pytz.utc),
+    ),
+    SamAssistanceListingData(
+        assistanceListingId="11.011",
+        title="Ocean Exploration",
+        status="Active",
+        publishedDate=datetime(2009, 10, 19, 6, 11, 33, tzinfo=pytz.utc),
+    ),
+    SamAssistanceListingData(
+        assistanceListingId="10.976",
+        title="Rice Production Program",
+        status="Active",
+        publishedDate=datetime(2023, 5, 23, 17, 33, 56, tzinfo=pytz.utc),
+    ),
+    SamAssistanceListingData(
+        assistanceListingId="93.NR1",
+        title="Nursing Research - Research Projects",
+        status="Active",
+        publishedDate=datetime(2026, 1, 30, 12, 0, 0, tzinfo=pytz.utc),
+    ),
+]
 
 
 def create_sam_gov_client(
@@ -23,10 +64,18 @@ def create_sam_gov_client(
 
     # Use mock client if use_mock is True in the config
     if sam_config.use_mock:
-        return MockSamGovClient(
+        logger.info("Using mocked sam.gov client")
+        client = MockSamGovClient(
             mock_data_file=sam_config.mock_data_file,
             mock_extract_dir=sam_config.mock_extract_dir,
         )
 
+        # Include a few default ALNs.
+        if sam_config.include_mock_alns:
+            client.add_mock_assistance_listings(MOCK_ASSISTANCE_LISTINGS)
+
+        return client
+
     # Otherwise use the real client
+    logger.info("Using real sam.gov client")
     return SamGovClient(sam_config)
--- api/tests/adapters/sam_gov/test_client.py
+++ api/tests/adapters/sam_gov/test_client.py
@@ -1,5 +1,6 @@
 """Tests for the SAM.gov API client."""
 
+import datetime
 import os
 from unittest import mock
 
@@ -7,9 +8,9 @@ import pytest
 import requests_mock
 from requests.exceptions import Timeout
 
-from src.adapters.sam_gov.client import SamGovClient
+from src.adapters.sam_gov.client import SamGovClient, SamGovError
 from src.adapters.sam_gov.config import SamGovConfig
-from src.adapters.sam_gov.models import SamExtractRequest
+from src.adapters.sam_gov.models import SamAssistanceListingRequest, SamExtractRequest
 
 
 class TestSamGovClient:
@@ -28,10 +29,7 @@ class TestSamGovClient:
     @pytest.fixture
     def client(self, config):
         """Fixture to create a test client."""
-        client = SamGovClient(config)
-        # Ensure the extract_url is None so that the client falls back to base_url/download
-        client.extract_url = None
-        return client
+        return SamGovClient(config)
 
     @mock.patch.dict(
         os.environ,
@@ -107,11 +105,8 @@ class TestSamGovClient:
             )
 
             # Call the client method, should raise an exception
-            with pytest.raises(Exception) as exc_info:
+            with pytest.raises(SamGovError, match="Failed to download extract: 404"):
                 client.download_extract(request, str(output_path))
-
-            # Verify the exception message
-            assert "Failed to download extract: 404" in str(exc_info.value)
 
     def test_download_extract_http_error(self, client, config, tmp_path):
         """Test handling of HTTP errors."""
@@ -130,11 +125,8 @@ class TestSamGovClient:
             )
 
             # Call the client method, should raise an exception
-            with pytest.raises(Exception) as exc_info:
+            with pytest.raises(SamGovError, match="Failed to download extract: 500"):
                 client.download_extract(request, output_path)
-
-            # Verify the exception message
-            assert "Failed to download extract: 500" in str(exc_info.value)
 
     # We skip this test because the retry logic would make it take several minutes
     # But it is left in case we want to test that the retries are working for timeouts
@@ -155,8 +147,79 @@ class TestSamGovClient:
             )
 
             # Call the client method, should raise a Timeout exception
-            with pytest.raises(Exception) as exc_info:
+            with pytest.raises(SamGovError, match="Request failed"):
                 client.download_extract(request, str(output_path))
 
-            # Verify the exception message
-            assert "Request failed" in str(exc_info.value)
+    def test_get_assistance_listing_success(self, client, config):
+
+        sam_gov_response = """{
+            "assistanceListingsData": [
+                {"assistanceListingId": "11.111", "title": "1st", "status": "Active", "publishedDate": "2026-07-03T05:00:02.401+00:00", "otherFields": []},
+                {"assistanceListingId": "22.222", "title": "2nd", "status": "Active", "publishedDate": "2026-06-03T05:00:02.401+00:00", "moreStuff": 123},
+                {"assistanceListingId": "33.333", "title": "3rd", "status": "Active", "publishedDate": "2026-07-02T05:00:02.401+00:00", "ignoredThings": "hello"}
+            ],
+            "pageNumber": 1,
+            "pageSize": 3,
+            "totalPages": 4,
+            "totalRecords": 11
+        }"""
+
+        with requests_mock.Mocker() as m:
+
+            m.get(
+                f"{config.base_url}/assistance-listings/v1/search?api_key={config.api_key}&pageNumber=1&pageSize=3",
+                text=sam_gov_response,
+            )
+
+            response = client.get_assistance_listings(
+                SamAssistanceListingRequest(page_size=3, page_number=1)
+            )
+
+            assert response.page_number == 1
+            assert response.page_size == 3
+            assert response.total_pages == 4
+            assert response.total_records == 11
+
+            assert len(response.assistance_listings_data) == 3
+
+            assert response.assistance_listings_data[0].assistance_listing_id == "11.111"
+            assert response.assistance_listings_data[0].title == "1st"
+            assert response.assistance_listings_data[0].status == "Active"
+            assert response.assistance_listings_data[0].published_date == datetime.datetime(
+                2026, 7, 3, 5, 0, 2, 401000, tzinfo=datetime.timezone.utc
+            )
+
+            assert response.assistance_listings_data[1].assistance_listing_id == "22.222"
+            assert response.assistance_listings_data[1].title == "2nd"
+            assert response.assistance_listings_data[1].status == "Active"
+            assert response.assistance_listings_data[1].published_date == datetime.datetime(
+                2026, 6, 3, 5, 0, 2, 401000, tzinfo=datetime.timezone.utc
+            )
+
+            assert response.assistance_listings_data[2].assistance_listing_id == "33.333"
+            assert response.assistance_listings_data[2].title == "3rd"
+            assert response.assistance_listings_data[2].status == "Active"
+            assert response.assistance_listings_data[2].published_date == datetime.datetime(
+                2026, 7, 2, 5, 0, 2, 401000, tzinfo=datetime.timezone.utc
+            )
+
+    def test_get_assistance_listing_429(self, client, config):
+        """Test handling of any 4xx errors, in this case a 429 rate limit error."""
+
+        with requests_mock.Mocker() as m:
+
+            m.get(
+                f"{config.base_url}/assistance-listings/v1/search?api_key={config.api_key}&pageNumber=1&pageSize=3",
+                status_code=429,
+                json={
+                    "code": "900804",
+                    "message": "Message throttled out",
+                    "description": "You have exceeded your quota .You can access API after 2026-Sep-03 00:00:00+0000 UTC",
+                    "nextAccessTime": "2026-Sep-03 00:00:00+0000 UTC",
+                },
+            )
+
+            with pytest.raises(SamGovError, match="Message throttled out"):
+                client.get_assistance_listings(
+                    SamAssistanceListingRequest(page_size=3, page_number=1)
+                )
