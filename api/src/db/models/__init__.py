41a6e284e51de6e76072b46252a40e2a76fc2f00
Daniel Ghazvini <daniel.ghazvini@reisystems.com>
2026-09-04 11:59:02 -0400

    Copy PendingFile and LkFileScanStatus Tables Over from Simpler

--- api/src/db/models/__init__.py
+++ api/src/db/models/__init__.py
@@ -1,5 +1,6 @@
 from . import (
     assistance_listing_models,
+    file_upload_models,
     grantor_organization_models,
     grantor_schema_table,
     lookup_models,
@@ -15,6 +16,7 @@ metadata = grantor_schema_table.metadata
 __all__ = [
     "metadata",
     "assistance_listing_models",
+    "file_upload_models",
     "lookup_models",
     "user_models",
     "grantor_organization_models",
